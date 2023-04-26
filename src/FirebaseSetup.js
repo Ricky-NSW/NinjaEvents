23cd8be029cf0919bb9886d1878de08ada4cd6b1
RickyH <rickyhobbs@acc.edu.au>
2023-04-26 19:38:35 +1000

    removed performance monitor - I dont have time to do it properly now

--- src/FirebaseSetup.js
+++ src/FirebaseSetup.js
@@ -7,7 +7,6 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 import 'firebase/compat/messaging';
 import 'firebase/storage';
-import { getPerformance } from "firebase/performance";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -21,9 +20,6 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
-// Initialize Performance Monitoring and get a reference to the service
-const perf = getPerformance(app);
 
 export const db = firebase.firestore();
 export const auth = firebase.auth();
